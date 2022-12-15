6fd37e049b4f295145388cba3868cb99bd142122
Lukas de Oliveira Biscaro <lukas.biscaro@hotmail.com>
2022-12-14 23:27:53 -0300

    Testing Dashboard Routes Problem.

--- src/index.js
+++ src/index.js
@@ -3,17 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { RecoilRoot } from 'recoil';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <RecoilRoot>
-    <Router>
-      <App />
-    </Router>
-  </RecoilRoot>
+  <Router>
+    <App />
+  </Router>
 
 );
 
