f9eebbc44ac46e53057263fff478f1f803a85c86
Lukas de Oliveira Biscaro <lukas.biscaro@hotmail.com>
2022-12-19 22:38:39 -0300

    Started Services Page - Client Side.

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -7,39 +7,36 @@ import Trusted6 from "./img/about-us.jpg";
 function AboutUs() {
   return (
     <div className="flex flex-col">
-        <Navbar />
-
-        <div className="flex min-h-screen">
-          <div className="flex flex-col items-center container mt-20 px-20">
-            <div className="relative">
-              <div className="relative mx-auto max-w-container px-4 pt-16 sm:px-6 lg:px-8 lg:pt-20">
-                <div className="mx-auto grid max-w-[40rem] grid-cols-1 gap-y-16 gap-x-8 lg:max-w-none lg:grid-cols-3">
-                  <div className="flex flex-col lg:pb-6">
-                    <h1 className="mt-4 text-4xl font-extrabold leading-none tracking-tight text-emerald-300 sm:text-5xl sm:leading-[3.5rem]">
-                      Callex Log
-                    </h1>
-                    <p className="order-first text-base font-semibold leading-7 text-gray-500">
-                      export with us
-                    </p>
-                    <p className="mt-6 text-base leading-7 text-gray-300">
-                      We offer the best solutions for exporting your goods
-                      Callex has a team specialized in exports, ready to provide full support to companies that wish to access the foreign market with total security and lower cost, observing all tax exemptions inherent to exports, guaranteed by law.
-                    </p>
-                  </div>
-                  <div className="relative lg:col-span-2 background-cover">
-                    <img src={Trusted6} alt="img" width="1600" height="1280" className="relative z-20 -mb-36 aspect-[853/482] max-w-[853px] rounded-xl sm:-mb-16 lg:-mb-8 xl:-mb-16"></img>
-                    </div>
-                  </div>
+      <Navbar />
+      <div className="flex min-h-screen">
+        <div className="flex flex-col items-center container mt-20 px-20">
+          <div className="relative">
+            <div className="relative mx-auto max-w-container px-4 pt-16 sm:px-6 lg:px-8 lg:pt-20">
+              <div className="mx-auto grid max-w-[40rem] grid-cols-1 gap-y-16 gap-x-8 lg:max-w-none lg:grid-cols-3">
+                <div className="flex flex-col lg:pb-6">
+                  <h1 className="mt-4 text-4xl font-extrabold leading-none tracking-tight text-emerald-300 sm:text-5xl sm:leading-[3.5rem]">
+                    Callex Log
+                  </h1>
+                  <p className="order-first text-base font-semibold leading-7 text-gray-500">
+                    export with us
+                  </p>
+                  <p className="mt-6 text-base leading-7 text-gray-300">
+                    We offer the best solutions for exporting your goods
+                    Callex has a team specialized in exports, ready to provide full support to companies that wish to access the foreign market with total security and lower cost, observing all tax exemptions inherent to exports, guaranteed by law.
+                  </p>
+                </div>
+                <div className="relative lg:col-span-2 background-cover">
+                  <img src={Trusted6} alt="img" width="1600" height="1280" className="relative z-20 -mb-36 aspect-[853/482] max-w-[853px] rounded-xl sm:-mb-16 lg:-mb-8 xl:-mb-16"></img>
                 </div>
               </div>
+            </div>
           </div>
         </div>
-
-        <Footer />
+      </div>
+      <Footer />
     </div>
   );
 }
 
 export default AboutUs;
 
- 
--- src/pages/Containers.js
+++ src/pages/Containers.js
@@ -6,19 +6,64 @@ import { Link } from 'react-router-dom'
 function Containers() {
   return (
     <div className="flex flex-col">
-        <Navbar />
+      <Navbar />
+      <div className="flex min-h-screen">
+        <div className="flex flex-col items-center container mt-5 px-10">
 
-        <div className="flex min-h-screen">
-          <div className="flex flex-col items-center container mt-20 px-20">
-            
-          </div>
+            <div className="container px-2 py-12 mx-auto">
+              <div className="flex flex-col text-center w-full mb-20">
+                <h1 className="text-2xl font-medium title-font mb-4 text-white tracking-widest">OUR SERVICES</h1>
+                <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+              </div>
+              <div className="flex flex-wrap -m-4 space-y-10">
+                <div className="p-4 lg:w-1/2">
+                  <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+                    <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://dummyimage.com/200x200" />
+                    <div className="flex-grow sm:pl-8">
+                      <h2 className="title-font font-medium text-lg text-white">Lorem Ipsum</h2>
+                      <h3 className="text-gray-500 mb-3">Location, Location - LC</h3>
+                      <p className="mb-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="p-4 lg:w-1/2">
+                  <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+                    <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://dummyimage.com/200x200" />
+                    <div className="flex-grow sm:pl-8">
+                      <h2 className="title-font font-medium text-lg text-white">Lorem Ipsum</h2>
+                      <h3 className="text-gray-500 mb-3">Location, Location - LC</h3>
+                      <p className="mb-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="p-4 lg:w-1/2">
+                  <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+                    <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://dummyimage.com/200x200" />
+                    <div className="flex-grow sm:pl-8">
+                      <h2 className="title-font font-medium text-lg text-white">Lorem Ipsum</h2>
+                      <h3 className="text-gray-500 mb-3">Location, Location - LC</h3>
+                      <p className="mb-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="p-4 lg:w-1/2">
+                  <div className="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
+                    <img alt="team" className="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4" src="https://dummyimage.com/200x200" />
+                    <div className="flex-grow sm:pl-8">
+                      <h2 className="title-font font-medium text-lg text-white">Lorem Ipsum</h2>
+                      <h3 className="text-gray-500 mb-3">Location, Location - LC</h3>
+                      <p className="mb-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
         </div>
-
-        <Footer />
+      </div>
+      <Footer />
     </div>
   );
 }
 
 export default Containers;
 
- 
