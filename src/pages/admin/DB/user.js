0445d30b63ddedfa833254c92f03a7a36a4bdc54
Lukas de Oliveira Biscaro <lukas.biscaro@hotmail.com>
2023-01-03 20:42:21 -0300

    Some small changes at Admin Dashboard.

--- src/pages/admin/DB/user.js
+++ src/pages/admin/DB/user.js
@@ -1,6 +1,6 @@
 export const user = {
-    name: 'Lukas Biscaro',
-    email: 'teste@teste.com.br',
+    name: 'Iron Hack',
+    email: 'ironhack@teste.com',
     imageUrl:
-        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        'https://thispersondoesnotexist.com/image',
 }
