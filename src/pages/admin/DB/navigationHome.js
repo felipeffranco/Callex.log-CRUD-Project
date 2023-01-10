2728a901e2bb210820e45ad939e55e548119e38c
Lukas de Oliveira Biscaro <lukas.biscaro@hotmail.com>
2023-01-10 20:43:11 -0300

    Created Menu for Mobile.

---
+++ src/pages/admin/DB/navigationHome.js
@@ -0,0 +1,6 @@
+export const navigationHome = [
+    { name: 'Home', to: '/' },
+    { name: 'About Us', to: '/about-us' },
+    { name: 'Containers', to: '/containers' },
+    { name: 'Services', to: '/services' },
+  ]
